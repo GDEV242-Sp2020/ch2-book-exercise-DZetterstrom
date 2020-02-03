324c7686c3b0e0c43e6f70bd719fd6ab68b0bb31
DZetterstrom <47015166+DZetterstrom@users.noreply.github.com>
2020-02-03 12:32:28 -0500

    2.83

--- Book.java
+++ Book.java
@@ -22,5 +22,11 @@ class Book
         title = bookTitle;
     }
 
-    // Add the methods here ...
+    public String getAuthor(){
+        return author;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
 }
